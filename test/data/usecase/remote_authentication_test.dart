a3be2419110d0c04668c0366c4c06f2df068e904
Emanoel S. Vieira <dev.emanoel@gmail.com>
2021-06-14 00:03:46 -0300

    add test of corret method

--- test/data/usecase/remote_authentication_test.dart
+++ test/data/usecase/remote_authentication_test.dart
@@ -6,26 +6,26 @@ class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
   Future<void> auth() async {
-    await httpClient.request(url);
+    await httpClient.request(url, "post");
   }
 
   RemoteAuthentication(this.httpClient, this.url);
 }
 
 abstract class HttpClient {
-  Future<void> ?request(String url) {}
+  Future<void> ?request(String url, String method) {}
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Corret url', () async {
+  test('Corret values', () async {
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
     final sut = RemoteAuthentication(httpClient, url);
 
     await sut.auth();
 
-    verify(httpClient.request(url));
+    verify(httpClient.request(url, "post"));
   });
 }
