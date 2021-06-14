821e6f582a03a835d0eac17d8a3acc0cad31f0cd
Emanoel S. Vieira <dev.emanoel@gmail.com>
2021-06-14 00:17:28 -0300

    add Authentication params

--- lib/domain/use_case/authentication.dart
+++ lib/domain/use_case/authentication.dart
@@ -1,5 +1,14 @@
+import 'package:completeapp/domain/use_case/use_case.dart';
+
 import '../entity/entity.dart';
 
 abstract class Authentication {
-  Future<Account> auth(String email,String password);
+  Future<Account> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String passwords;
+
+  AuthenticationParams(this.email, this.passwords);
 }
